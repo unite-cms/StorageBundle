991964c6ae40ba67a81a9152669f267599570dd6
stefan-kamsker <stefan.kamsker@virtual-identity.com>
2018-04-13 17:03:29 +0200

    fix #19 [CoreBundle,StorageBundle,CollectionFieldbundle] Rename UnitedCMS to UniteCMS

--- DependencyInjection/UniteCMSStorageExtension.php
+++ DependencyInjection/UniteCMSStorageExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace UnitedCMS\StorageBundle\DependencyInjection;
+namespace UniteCMS\StorageBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class UnitedCMSStorageExtension extends Extension
+class UniteCMSStorageExtension extends Extension
 {
     /**
      * {@inheritdoc}
